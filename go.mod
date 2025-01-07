ea993909dcfaaa6db450be3e2e73ff1c5bfa037f
immanuel-254 <njugunaimmanuel1@outlook.com>
2025-01-07 15:20:12 +0300

    connect and migrating to database

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/immanuel-254/myauth
 
 go 1.23.3
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-sqlite3 v1.14.24
+	github.com/pressly/goose/v3 v3.24.0
+)
+
+require (
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/pressly/goose/v3"
+)
+
+func main() {
+	// connect to db
+	db, err := sql.Open("sqlite3", os.Getenv("DB"))
+	if err != nil {
+		log.Fatalf("%s", err.Error())
+	}
+	defer func() {
+		if closeError := db.Close(); closeError != nil {
+			fmt.Println("Error closing database", closeError)
+			if err == nil {
+				err = closeError
+			}
+		}
+	}()
+
+	// migrate to database
+	goose.SetDialect("sqlite3")
+
+	// Apply all "up" migrations
+	err = goose.Up(db, "internal/migrations")
+	if err != nil {
+		log.Fatalf("Failed to apply migrations: %v", err)
+	}
+
+	log.Println("Migrations applied successfully!")
+}
